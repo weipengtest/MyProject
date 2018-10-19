f11ab9b93cc6370edb27d1236b4b43e100e7b0b7
weipengtest <1009961778@qq.com>
2018-10-19 10:16:38 +0800

    update Test1 10:16

--- src/com/test/Test1.java
+++ src/com/test/Test1.java
@@ -4,21 +4,21 @@ package com.test;
 public class Test1 {
 	//weipeng003
 	private String a;
-
+	//weipeng003
 	private String b;
-	
+	//weipeng003
 	public String getA() {
 		return a;
 	}
-
+	//weipeng003
 	public void setA(String a) {
 		this.a = a;
 	}
-
+	//weipeng003
 	public String getB() {
 		return b;
 	}
-
+	//weipeng003
 	public void setB(String b) {
 		this.b = b;
 	}
